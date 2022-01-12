eb7538a2bb437c4e8f9b88ee6241dbaead9cbba1
Dishu Bansal <dishu.bansal@ryerson.ca>
2022-01-12 00:16:16 -0500

    Day 5

--- Neural Network Classification.py
+++ Neural Network Classification.py
@@ -2,27 +2,63 @@ from sklearn.datasets import make_circles
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
+
 
 n = 1000
 
 X, y = make_circles(n_samples=n, noise=0.03, random_state=42)
 
-circles = pd.DataFrame({"X0": X[:, 0], "X1": X[:, 1], "label" : y})
-
-print(circles.head())
+# circles = pd.DataFrame({"X0": X[:, 0], "X1": X[:, 1], "label" : y})
 
 # plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu)
 # plt.show()
+
+X_train = X[:800]
+X_test = X[800:]
+y_train = y[:800]
+y_test = y[800:]
+
+def plot_decision_boundary(model, X, y):
+    x_min, x_max = X[:, 0].min() - 0.1, X[:,0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:,1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                         np.linspace(y_min, y_max, 100))
+    x_in = np.c_[xx.ravel(), yy.ravel()]
+    
+    y_pred = model.predict(x_in)
+    
+    if len(y_pred[0]) > 1:
+        print("Doing Multiclass Classification")
+        y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+    else:
+        print("Doing Binary Classification")
+        y_pred = np.round(y_pred).reshape(xx.shape)
+    
+    plt.contourf(xx,yy,y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:,0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
 
 tf.random.set_seed(42)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(100, activation="relu"),
-    tf.keras.layers.Dense(1)
+    tf.keras.layers.Dense(1, activation="sigmoid")
     ])
 
 model.compile(loss=tf.keras.losses.binary_crossentropy,
-              optimizer=tf.keras.optimizers.SGD(),
+              optimizer=tf.keras.optimizers.Adam(),
               metrics=["accuracy"])
 
-model.fit(X, y, epochs=200)
+model.fit(X_train, y_train, epochs=200)
+
+model.evaluate(X_test, y_test)
+
+plt.figure(figsize=(12,6))
+plt.subplot(1,2,1)
+plt.title("Train")
+plot_decision_boundary(model, X=X_train, y=y_train)
+plt.subplot(1,2,2)
+plot_decision_boundary(model, X=X_test, y=y_test)
+plt.show()
